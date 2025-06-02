84d0b5152885cf87582a81e316d0ba26c78c89b3
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-06-02 16:16:46 -0300

    feat(api/pacientes): adiciona controller para gerenciamento de pacientes  -Implementa endpoint GET para listar pacientes ativos com paginação  -Adiciona endpoint POST para cadastro de pacientes com validação  -Inclui endpoint PUT para atualização de dados de pacientes  -Adiciona endpoint DELETE para inativação de pacientes via coluna ativo

---
+++ src/main/java/Med/voll/api/controller/PacienteController.java
@@ -0,0 +1,57 @@
+package med.voll.api.controller;
+
+
+import jakarta.validation.Valid;
+import med.voll.api.paciente.Paciente;
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import med.voll.api.repository.PacienteRepository;
+import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
+import org.springframework.data.web.PageableDefault;
+import med.voll.api.paciente.DadosAtualizacaoPaciente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/paciente")
+public class PacienteController {
+    
+    @Autowired
+    private PacienteRepository repository;
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = "nome") Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPaciente::new);
+    }
+
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
+        repository.save(new Paciente(dados));
+    }
+    
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoPaciente dados) {
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void remover(@PathVariable Long id) {
+        var paciente = repository.getReferenceById(id);
+        paciente.inativar();
+}
+}
