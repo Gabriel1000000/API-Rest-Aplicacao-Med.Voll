ca6ebc8e13fec37dcb63be06e2ddad4ac2294fb4
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-06-04 15:25:04 -0300

    feat: adiciona tratamento global para erro 404 com @RestControllerAdvice

---
+++ src/main/java/Med/voll/api/infra/TratadorDeErros.java
@@ -0,0 +1,17 @@
+package med.voll.api.infra;
+
+
+import org.springframework.http.ResponseEntity;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// anoteichon que infomar ao spring que essa clesse vai tratrar erros na api.
+@RestControllerAdvice
+public class TratadorDeErros {
+    
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity TratarErro404(){
+        return ResponseEntity.notFound().build();
+    }
+}
