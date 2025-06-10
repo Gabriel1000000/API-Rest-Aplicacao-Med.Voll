f477ec56dc5de20f6c90c02387857aef58c19e82
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-06-10 18:23:09 -0300

    feat(security): adicionar beans de AuthenticationManager e PasswordEncoder
    Inclui os beans AuthenticationManager e PasswordEncoder na classe de configuração
    SecurityConfigurations para permitir autenticação de usuários e codificação de senhas
    usando BCrypt.

--- src/main/java/Med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/Med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,20 +2,38 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+
+    //O @Bean, serve para exportar uma classe para spring fazendo com que ele consiga carregá-la e realize a sua injeção de dependencia em outras classes.
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return  http
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .build();
+    }
+
+    @Bean
+    public AuthenticationManager  authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
