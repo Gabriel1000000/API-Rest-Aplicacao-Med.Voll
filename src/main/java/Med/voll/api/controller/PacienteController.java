828be391c6d923ccb18a2d14c68d6101e078a774
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-06-20 22:08:59 -0300

    refactor(controller-paciente): reorganizar DTOs e entidade Paciente para domain.paciente e incluir anotações @Tag e @SecurityRequirement para documentação OpenAPI

--- src/main/java/Med/voll/api/controller/PacienteController.java
+++ src/main/java/Med/voll/api/controller/PacienteController.java
@@ -2,15 +2,20 @@ package med.voll.api.controller;
 
 
 import jakarta.validation.Valid;
-import med.voll.api.paciente.Paciente;
+// import med.voll.api.paciente.Paciente;
 import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import med.voll.api.domain.paciente.DadosAtualizacaoPaciente;
+import med.voll.api.domain.paciente.DadosCadastroPaciente;
+import med.voll.api.domain.paciente.DadosListagemPaciente;
+import med.voll.api.domain.paciente.Paciente;
 import med.voll.api.repository.PacienteRepository;
-import med.voll.api.paciente.DadosCadastroPaciente;
-import med.voll.api.paciente.DadosListagemPaciente;
+// import med.voll.api.paciente.DadosCadastroPaciente;
+// import med.voll.api.paciente.DadosListagemPaciente;
 import org.springframework.data.web.PageableDefault;
-import med.voll.api.paciente.DadosAtualizacaoPaciente;
+// import med.voll.api.paciente.DadosAtualizacaoPaciente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,9 +26,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RestController
 @RequestMapping("/paciente")
+@Tag(name = "Pacientes", description = "Operações para cadastrar pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
     
     @Autowired
