fb9a94a418be92e240980b2026e36b82030eea9e
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-06-20 22:20:49 -0300

    refactor(token-service): mover entidade Usuario para pacote domain.usuario

--- src/main/java/Med/voll/api/infra/security/TokenService.java
+++ src/main/java/Med/voll/api/infra/security/TokenService.java
@@ -12,7 +12,9 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 
-import med.voll.api.usuario.Usuario;
+import med.voll.api.domain.usuario.Usuario;
+
+
 
 @Service
 public class TokenService {
