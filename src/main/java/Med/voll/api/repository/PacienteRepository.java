c11cc248ea3f0488d5f54d247c7e5dfdb848b588
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-06-20 22:26:48 -0300

    feat(repository): adicionar método para verificar status ativo do paciente no PacienteRepository

--- src/main/java/Med/voll/api/repository/PacienteRepository.java
+++ src/main/java/Med/voll/api/repository/PacienteRepository.java
@@ -1,14 +1,21 @@
 package med.voll.api.repository;
 
 
-import med.voll.api.paciente.Paciente;
+
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import med.voll.api.domain.paciente.Paciente;
+
 
 public interface PacienteRepository extends JpaRepository <Paciente, Long> {
 
     Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("SELECT p.ativo FROM Paciente p WHERE p.id = :idPaciente")
+    Boolean findAtivoById(Long idPaciente);
     
 }
